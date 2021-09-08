865d62fb9fc273d3c9dbeb481faf55ff985b57f7
Rashid Wassan <rashidwassaan@gmail.com>
2021-09-08 18:27:53 +0500

    fixed label

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Flutter Shapes',
       theme: ThemeData(
         primarySwatch: Colors.teal,
       ),
